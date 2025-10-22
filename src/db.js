b18d424d829d06c1d6a9a81b27c9fd3cc1589588
Bruno Mota <bmotaluiz@gmail.com>
2025-10-21 23:07:14 -0300

    Corrige sqlite3 build e caminho para Render

--- src/db.js
+++ src/db.js
@@ -1,12 +1,18 @@
-const knex = require('knex');
-const path = require('path');
+import knex from "knex";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const dbPath = path.resolve(__dirname, "../data/database.sqlite");
 
 const db = knex({
-  client: 'sqlite3',
+  client: "sqlite3",
   connection: {
-    filename: path.resolve(__dirname, '../data/database.sqlite'),
+    filename: dbPath
   },
-  useNullAsDefault: true,
+  useNullAsDefault: true
 });
 
-module.exports = db;
+export default db;
